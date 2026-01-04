d3454401459c5d6808aaada87d906d5b6a49e577
gubard <maksimovsergey08@gmail.com>
2026-01-04 12:56:24 +0200

    Clean up: Remove unused using directives in DioclesServiceProvider.cs

--- Diocles/Services/DioclesServiceProvider.cs
+++ Diocles/Services/DioclesServiceProvider.cs
@@ -1,15 +1,6 @@
-﻿using System.Text.Json;
-using Diocles.Models;
-using Diocles.Ui;
-using Gaia.Helpers;
-using Gaia.Models;
-using Gaia.Services;
-using Hestia.Contract.Models;
+﻿using Diocles.Ui;
 using Hestia.Contract.Services;
-using Inanna.Models;
-using Inanna.Services;
 using Jab;
-using Nestor.Db.Services;
 
 namespace Diocles.Services;
 
