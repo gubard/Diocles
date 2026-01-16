9e9b71e3846d8dfbfb73bf035431eac8cd41a83f
Serhii Maksymov <maksimovsergey08@gmail.com>
2026-01-16 23:00:30 +0200

    Updated `ItemsControl.axaml` tooltip binding and added platform-specific description parameters in `DiocleHelper`.

--- Diocles/Helpers/DiocleHelper.cs
+++ Diocles/Helpers/DiocleHelper.cs
@@ -4,5 +4,6 @@ namespace Diocles.Helpers;
 
 public static class DiocleHelper
 {
-    public static readonly StringCutParameters DescriptionStringParameters = new(5, 100);
+    public static readonly StringCutParameters DesktopDescriptionStringParameters = new(5, 100);
+    public static readonly StringCutParameters AndroidDescriptionStringParameters = new(1, 20);
 }
