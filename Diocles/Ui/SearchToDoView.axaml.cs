602f6915e3c2fd07e66473aa4b4dc2ab3e4a767e
gubard <maksimovsergey08@gmail.com>
2026-02-20 21:26:36 +0200

    Add focus to the search text box on view load

--- Diocles/Ui/SearchToDoView.axaml.cs
+++ Diocles/Ui/SearchToDoView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Diocles.Models;
+using Inanna.Helpers;
 
 namespace Diocles.Ui;
 
@@ -8,6 +9,7 @@ public sealed partial class SearchToDoView : UserControl, IToDosView
     public SearchToDoView()
     {
         InitializeComponent();
+        Loaded += (_, _) => SearchTextTextBox.FocusCaretIndex();
     }
 
     public IToDosViewModel ViewModel =>
